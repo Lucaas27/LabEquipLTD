baab0d0d1e34b6033ab581dd0770bf058f7cd06e
Lucas Gomes <lucasggomees@gmail.com>
2023-11-21 13:54:49 +0000

    Added colour palette to the website

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,13 @@
 export default {
   content: ['./src/**/*.{html,js,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'bg-primary': '#f25454',
+        'bg-secondary': '#eff5ff',
+        'bg-dark': '#1b2c51',
+      },
+    },
   },
   plugins: [],
 };
