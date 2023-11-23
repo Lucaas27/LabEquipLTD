e1e0fd3305ea68f115d313f3ea2a3e682870e6a0
Lucas Gomes <lucasggomees@gmail.com>
2023-11-23 12:55:25 +0000

    Removed alpinejs - opted for using typescript only

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,3 +1,60 @@
-import Alpine from 'alpinejs';
+function initApp() {
+  // **** hambugerBtnHandler ***
 
-Alpine.start();
+  function hambugerBtnHandler() {
+    const hambugerBtn: HTMLElement | null =
+      document.getElementById('hamburger-button');
+    const mobileMenu: HTMLElement | null =
+      document.getElementById('mobile-menu');
+
+    const toggleStates = (el: HTMLElement | null) => () => {
+      if (el) {
+        el.classList.toggle('hidden');
+        el.classList.toggle('flex');
+        hambugerBtn?.classList.toggle('toggle-btn');
+      }
+    };
+
+    hambugerBtn?.addEventListener('click', toggleStates(mobileMenu));
+    mobileMenu?.addEventListener('click', toggleStates(mobileMenu));
+  }
+
+  // **** dropdownNavBarHandler ***
+
+  function dropdownNavBarHandler() {
+    let closeTimeout: NodeJS.Timeout;
+    const dropdownNavBtn: HTMLElement | null =
+      document.getElementById('nav-dropdown-btn');
+    const dropdownNavItems: HTMLElement | null =
+      document.getElementById('nav-dropdown-items');
+
+    dropdownNavBtn?.addEventListener(
+      'mouseenter',
+      () => dropdownNavItems?.classList.replace('hidden', 'flex')
+    );
+
+    dropdownNavBtn?.addEventListener('mouseleave', () => {
+      // Set a timeout to close the dropdown after a short delay
+      closeTimeout = setTimeout(() => {
+        dropdownNavItems?.classList.replace('flex', 'hidden');
+      }, 200);
+    });
+
+    // Cancel the timeout and keep the dropdown open if the mouse enters the dropdown
+    dropdownNavItems?.addEventListener('mouseenter', () => {
+      clearTimeout(closeTimeout);
+    });
+
+    // Resume the timeout if the mouse leaves the dropdown
+    dropdownNavItems?.addEventListener('mouseleave', () => {
+      closeTimeout = setTimeout(() => {
+        dropdownNavItems?.classList.replace('flex', 'hidden');
+      }, 200);
+    });
+  }
+
+  dropdownNavBarHandler();
+  hambugerBtnHandler();
+}
+
+document.addEventListener('DOMContentLoaded', initApp);
