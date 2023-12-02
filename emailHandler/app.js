20564f9ecf7391189d1b5f4a835656c2903e7179
Lucas Gomes <lucasggomees@gmail.com>
2023-12-02 21:29:55 +0000

    Fix the cors config

--- emailHandler/app.js
+++ emailHandler/app.js
@@ -1,26 +1,20 @@
 /* eslint-disable import/extensions */
-import dotenv from 'dotenv';
-import cors from 'cors';
-import express from 'express';
-import { sendEmail } from './emailHandler.js';
+import dotenv from "dotenv";
+import cors from "cors";
+import express from "express";
+import { sendEmail } from "./emailHandler.js";
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
-app.use(
-  cors({
-    // origin: `http://localhost:${process.env.serverPort}`,
-    origin: `http://localhost:5333`,
-    optionsSuccessStatus: 200,
-  })
-);
+app.use(cors());
 app.use(express.json());
 app.listen(process.env.serverPort, () => {
-  console.log('Server is running on port', process.env.serverPort);
+  console.log("Server is running on port", process.env.serverPort);
 });
 
-app.post('/contactForm/sendEmail', (req, res) => {
+app.post("/contactForm/sendEmail", (req, res) => {
   sendEmail(req, res);
 });
