1bbe12124e89cccea2e545a26dbf20c98c80b0b9
Lucas Gomes <lucasggomees@gmail.com>
2024-02-22 00:47:25 +0000

    New form added in the services page

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -2,6 +2,7 @@ import "aos/dist/aos.css";
 import "keen-slider/keen-slider.min.css";
 import AOS from "aos";
 import KeenSlider from "keen-slider";
+import { Plugin } from "intl-tel-input";
 
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
@@ -9,6 +10,8 @@ function initApp() {
   const keenSliderEl = document.getElementById("keen-slider");
   const keenSliderPrevious = document.getElementById("keen-slider-previous");
   const keenSliderNext = document.getElementById("keen-slider-next");
+  const phoneInputField = document.querySelector("#reportContactNumber");
+  let phoneInput: Plugin;
 
   const keenSliderPreviousDesktop = document.getElementById(
     "keen-slider-previous-desktop",
@@ -40,6 +43,9 @@ function initApp() {
 
       // Get all products
       const products = document.querySelectorAll<HTMLDivElement>(".product");
+      const productImage = document.getElementById(
+        "productImage",
+      ) as HTMLImageElement;
 
       // Show all products if "All" is selected
       if (!category || category.toLowerCase() === "all") {
@@ -55,6 +61,8 @@ function initApp() {
       if (productTitle) {
         const cat = category;
         productTitle.innerHTML = `Products in ${cat}`;
+        const imgName = cat.split(" ").join("").toLowerCase();
+        productImage!.srcset = `img/products/${imgName}.webp`;
       }
 
       // Show all matching products, hide others
@@ -210,6 +218,175 @@ function initApp() {
     }
   }
 
+  function closePopupForm(event: Event) {
+    const targetElement = event.target as Element;
+
+    // Check if the click event occurred inside the form
+    if (!targetElement?.closest(".popup-content")) {
+      // Hide the overlay and form
+      const overlay = document.getElementById("services-popup");
+      overlay?.classList.remove("fixed");
+      overlay?.classList.remove("flex");
+      overlay?.classList.add("hidden");
+      overlay?.setAttribute("aria-hidden", "true");
+
+      // Remove event listener to close the popup when clicking outside the form
+      overlay?.removeEventListener("click", closePopupForm);
+
+      // Allow scrolling of the background content
+      document.body.classList.remove("overflow-hidden");
+    }
+  }
+
+  function openPopupForm() {
+    // Show the overlay and form
+    const overlay = document.getElementById("services-popup");
+    overlay?.classList.remove("hidden");
+    overlay?.classList.add("fixed");
+    overlay?.classList.add("flex");
+    overlay?.setAttribute("aria-hidden", "false");
+    overlay?.focus(); // Set focus to the overlay for accessibility
+
+    // Add event listener to close the popup when clicking outside the form
+    overlay?.addEventListener("click", closePopupForm);
+
+    // Prevent scrolling of the background content when the popup is open
+    document.body.classList.add("overflow-hidden");
+  }
+
+  // Add event listener to open the popup form when clicking on a specific element
+  const openPopupButton = document.getElementById("open-popup-button");
+  openPopupButton?.addEventListener("click", openPopupForm);
+
+  // Add event listener to close the popup form when clicking on the close button
+  const closePopupButton = document.getElementById("popup-close");
+  closePopupButton?.addEventListener("click", closePopupForm);
+
+  function reportIssueFormHandler() {
+    phoneInput = window.intlTelInput(phoneInputField!, {
+      preferredCountries: ["tz", "us", "gb"],
+      utilsScript:
+        "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
+    });
+
+    // form
+    const reportIssueForm = document.getElementById(
+      "reportIssue",
+    ) as HTMLFormElement;
+
+    // notifications
+    const notifications = document.getElementById("notification");
+
+    reportIssueForm?.addEventListener("submit", (e: any) => {
+      e.preventDefault();
+
+      const formData = new FormData(e.target);
+
+      // Remove the original phone input field from the FormData object
+      formData.delete("reportContactNumber");
+
+      // Append the formatted phone number to the FormData object
+      formData.append("phone", phoneInput.getNumber());
+
+      // Convert FormData to plain JavaScript object
+      const formProps: Record<string, string> = {};
+
+      formData.forEach((value, key) => {
+        formProps[key] = value.toString();
+      });
+
+      // Hide the overlay and form
+      const overlay = document.getElementById("services-popup");
+
+      overlay?.classList.remove("fixed");
+      overlay?.classList.remove("flex");
+      overlay?.classList.add("hidden");
+      overlay?.setAttribute("aria-hidden", "true");
+      // Allow scrolling of the background content
+      document.body.classList.remove("overflow-hidden");
+
+      fetch(`php/reportIssue.php`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        // Convert data to URL-encoded form data
+        body: new URLSearchParams(formProps).toString(),
+      })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          return response.json();
+        })
+        .then((resData) => {
+          if (resData.success) {
+            // Reset form
+            reportIssueForm?.reset();
+
+            if (notifications) {
+              notifications.classList.remove("hidden");
+              notifications.innerHTML = `
+              <div
+                class="mb-3 inline-flex items-center rounded-lg bg-green-100 px-6 py-5 text-base text-green-700 z-50"
+                role="alert"
+              >
+                <span class="mr-2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    class="h-5 w-5"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
+                      clip-rule="evenodd" />
+                  </svg>
+                </span>
+                ${resData.message}
+              </div>
+                `;
+
+              setTimeout(() => {
+                notifications.innerHTML = "";
+              }, 3000);
+            }
+          } else if (notifications) {
+            notifications.classList.remove("hidden");
+            notifications.innerHTML = `
+              <div
+                class="mb-3 inline-flex items-center rounded-lg bg-red-100 px-6 py-5 text-base text-red-700 z-50"
+                role="alert"
+              >
+                <span class="mr-2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    class="h-5 w-5"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm-1.72 6.97a.75.75 0 10-1.06 1.06L10.94 12l-1.72 1.72a.75.75 0 101.06 1.06L12 13.06l1.72 1.72a.75.75 0 101.06-1.06L13.06 12l1.72-1.72a.75.75 0 10-1.06-1.06L12 10.94l-1.72-1.72z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                </span>
+                ${resData.message}
+              </div>
+              `;
+
+            setTimeout(() => {
+              notifications.innerHTML = "";
+            }, 3000);
+          }
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    });
+  }
   // **** dropdownNavBarHandler ***
 
   // function dropdownNavBarHandler() {
@@ -424,6 +601,7 @@ function initApp() {
   sliderHandler();
   scrollToProducts();
   if (keenSliderEl) testimonialHandler();
+  if (phoneInputField) reportIssueFormHandler();
   accordionHandler();
   productClickHandler();
 }
